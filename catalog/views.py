8bf842159f65ce6cf70a32be7d28db3a8831b8c3
Linch <dr.linch19@gmail.com>
2023-12-05 19:11:13 +0300

    Home work 20.2

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+from catalog.models import Product, Category
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list,
+    }
+    return render(request, 'catalog/index.html', context)
 
 
 def contacts(request):
     return render(request, 'catalog/contact.html')
 
+
+def product_info(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'object': product
+    }
+    return render(request, 'catalog/product.html', context)
